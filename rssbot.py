677c15791ad210bce65df848d59e3d86832a9241
elektr0ninja <elektr0ninja@users.noreply.github.com>
2025-05-25 14:27:47 +0200

    Create rssbot.py
    initial version

---
+++ rssbot.py
@@ -0,0 +1,50 @@
+#! /usr/bin/python3
+
+import sys
+import feedparser
+import socket
+import subprocess
+from datetime import datetime
+import urllib.parse
+import requests
+
+###########################
+# Config section
+feeds = ("https://sample1.com/rss.xml",
+        "https://sample2.com/rss.xml")
+marker_file = "marker.dat"
+BOT_TOKEN = "PUT_YOUR_BOT_TOKEN_HERE"
+chat_id = "CHAT_ID_FOR_YOUR_USER"
+keyword = "KEYWORD_TO_GET_NOTIFIED_ABOUT"
+###########################
+
+# Load last check time from the marker file
+
+with open(marker_file,'r') as marker:
+    try:
+        lastcheck = datetime.strptime(marker.read(),"%d/%m/%Y %H:%M:%S")
+    except:
+        lastcheck = datetime.now()
+
+# Parse the feeds
+
+for f in feeds:
+    d = feedparser.parse(f)
+    print (f"Parsing feed {f} for new entries...")
+    for s in d.entries:
+        pub = s.published.split(',')[1].split()
+        publishedstr = f"{pub[0]} {pub[1]} {pub[2]} {pub[3]}"
+        published = datetime.strptime(publishedstr,"%d %b %Y %H:%M:%S")
+        if published > lastcheck:
+            print(f" New item found: {s.title} {s.link}")
+            if keyword in s.title:
+                message = urllib.parse.quote_plus(f"RSSbot: {s.title} {s.link}")
+                url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+                requests.get(url)
+
+# Store the current time to save last check time...
+
+now = datetime.now()
+
+with open(marker_file,'w') as marker:
+    marker.write(now.strftime("%d/%m/%Y %H:%M:%S"))
